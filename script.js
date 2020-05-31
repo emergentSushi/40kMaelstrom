f92b3b5949be703e508c16a5b7303c6e551fd8f2
Mathew Peachey <mathew.peachey@live.com>
2020-05-31 23:56:10 +1200

    inlining dependency removal

--- script.js
+++ script.js
@@ -1,52 +1,78 @@
-function getRandomInt(min, max) {
+Math.randomRange = function(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
-$(document).ready(function() {
-    renderFor($('#option').val());
 
-    $('#pick').click(function() {
+HTMLElement.prototype.empty = function() {
+    this.innerHTML = '';
+}
+
+HTMLElement.prototype.on = function(event, cb) {
+    this.addEventListener(event, cb);
+}
+
+if (!HTMLElement.prototype.remove) {
+    HTMLElement.prototype.remove = function () {
+        this.parentNode.removeChild(this);
+    }
+}
+
+$ = function(query) {
+    var nodes = document.querySelectorAll(query);
+    if (nodes.length === 1) {
+        return nodes[0];
+    }
+    else return nodes;
+}
+
+document.addEventListener( "DOMContentLoaded", function(){
+    renderFor($('#option').value.toString());
+
+    $('#pick').on('click', function() {
         var o = $('.obj');
-        var c = $(o[getRandomInt(0, o.length)]).clone();
-        c.click(function() {
-            c.addClass('hidden');
-            setTimeout(() => c.remove(), 100);
+        var c = o[Math.randomRange(0, o.length)].cloneNode(true);
+        c.on('click', function() {
+            c.remove();
         });
-        $('#dump').append(c);
+        $('#dump').appendChild(c);
     });
 
-    $('#clear').click(function() {
+    $('#clear').on('click', function() {
         $('#dump').empty();
     });
 
-    $('#option').change(function() {
-        renderFor($(this).val());
+    $('#option').on('change', function() {
+        renderFor(this.value.toString());
     });
 });
 
 function rebindEvents() {
-    $('.obj').click(function() {
-        var c = $(this).clone();
-        c.click(function() {
-            c.addClass('hidden');
-            setTimeout(() => c.remove(), 100);
-        });
+    var objectives = $('.obj');
+    for (var i = 0; i < objectives.length; i++) {
+        objectives[i].on('click', function() {
+            var c = this.cloneNode(true);
+            c.on('click', function() {
+                c.remove();
+            });
 
-        $('#dump').append(c);
-    });
+            $('#dump').appendChild(c);
+        });
+    }
 }
 
 function renderFor(army) {
     var data = eval(army).concat(common);
     var root = $('#obj-wrapper');
     $('#dump').empty();
-    $(root).empty();
+    root.empty();
     var objectives = [];
     for (var i = 0; i < data.length; i++) {
         objectives.push(render(data[i], 'objective'));
     }
-    root.append(objectives);
+
+    var documentFragment = document.createRange().createContextualFragment(objectives.join(''));
+    root.appendChild(documentFragment);
     rebindEvents();
 }
 
